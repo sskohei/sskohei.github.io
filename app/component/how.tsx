c534beff71b193e8e540a36088d4498491e703aa
sskohei <k.kuro.2007@gmail.com>
2025-11-04 15:48:56 +0900

    update

--- app/component/how.tsx
+++ app/component/how.tsx
@@ -3,6 +3,13 @@ import { useState } from "react"
 
 export default function How(){
     return (
-        <div className="flex bg-white text-black"></div>
+        <div className="bg-white text-black">
+            <h1 className="flex gap-3 text-xl font-bold">オンライン絵馬の使い方</h1>
+            <div className="flex justify-center gap-3">
+                <p>　このサイトは神社に行かずとも絵馬の疑似体験を目指したサイトです。ホーム画面の+ボタンを押すことで誰でも絵馬を掛けることができます。<br />
+                    ほかの人が書いた絵馬を読むことも可能です。
+                </p>
+            </div>
+        </div>
     )
 }
--- app/how/page.tsx
+++ app/how/page.tsx
@@ -3,7 +3,7 @@ import Head from "../component/header"
 import How from "../component/how"
 
 export default function Page(){
-    return(<div>
+    return(<div className="bg-white w-full h-full w-screen h-screen">
         <Head/>
         <How/>
     </div>)
